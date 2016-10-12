d98a2f7938ee1ecca26f0fc415c2ec78b4034343
hikaruu <no.noo..nooo.yes@gmail.com>
2016-10-12 20:29:06 +0700

    ahihi

--- app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
+++ app/src/main/java/apidez/com/lab1/adapter/PostAdapter.java
@@ -2,14 +2,18 @@ package apidez.com.lab1.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
+import apidez.com.lab1.R;
 import apidez.com.lab1.model.Post;
 
 /**
@@ -34,12 +38,32 @@ public class PostAdapter extends ArrayAdapter<Post> {
     public View getView(int position, View convertView, ViewGroup parent) {
         // TODO: Insert your code
         // bindViewHolder(position, viewHolder)
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.item_post, parent, false);
+            ViewHolder viewHolder = new ViewHolder();
+            viewHolder.ivAvatar = (ImageView) convertView.findViewById(R.id.ivAvatar);
+            viewHolder.ivImage = (ImageView) convertView.findViewById(R.id.ivImage);
+            viewHolder.tvUsername = (TextView) convertView.findViewById(R.id.tvUsername);
+            viewHolder.tvDate = (TextView) convertView.findViewById(R.id.tvDate);
+
+            viewHolder.tvDescription = (TextView) convertView.findViewById(R.id.tvDescription);
+
+
+
+        }
         return convertView;
     }
 
     private void bindViewHolder(int position, ViewHolder viewHolder) {
         Post post = mPosts.get(position);
         // TODO: Insert your code
+        viewHolder.tvUsername.setText(post.getUsername());
+        viewHolder.tvDescription.setText(post.getDescription());
+        viewHolder.tvDate.setText(post.getDate());
+
+        Picasso.with(getContext()).load(post.getAvatar()).into(viewHolder.ivAvatar);
+        Picasso.with(getContext()).load(post.getImage()).into(viewHolder.ivImage);
     }
 
     private void loadImage(ImageView imageView, String path) {
